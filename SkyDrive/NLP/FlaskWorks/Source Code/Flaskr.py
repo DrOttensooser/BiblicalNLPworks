59e0304ee8e739aeabd13215b3f1d9193e7b9254
DrOttensooser <avner.ottensooser@gmail.com>
2012-07-24 06:00:29 +1000

    Work on the web site

---
+++ SkyDrive/NLP/FlaskWorks/Source Code/Flaskr.py
@@ -0,0 +1,80 @@
+'# -*- coding: utf-8 -*-'
+from __future__ import division
+
+''' This module alalyses Shakespeare's Documents The module should be called after
+actibvating the module GatherFiles.PY '''
+
+__author__ = 'Dr Avner OTTENSOOSER <avner.ottensooser@gmail.com>'
+__version__ = '$Revision: 0.01 $'
+
+
+AO_ROOT_PATH         = 'C:\\Users\\Avner\\SkyDrive\\NLP\\'
+AO_PROJECT_NAME      = 'FlaskWorks'
+AO_DATABASE          = AO_ROOT_PATH + AO_PROJECT_NAME + '\\Data\\Database\\flaskr.db'
+
+import sqlite3
+from werkzeug.wrappers import Request, Response
+from flask import Flask, request, session, g, redirect, url_for, \
+abort, render_template, flash
+# configuration
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
+
+
+# create our little application :)
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+def connect_db():
+    return sqlite3.connect(app.config['AO_DATABASE'])
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+    g.db.close()
+
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    g.db.execute('insert into entries (title, text) values (?, ?)',[request.form['title'], request.form['text']])
+    g.db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
+
+
+@app.route('/')
+def show_entries():
+    cur = g.db.execute('select title, text from entries order by id desc')
+    entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+    return render_template('show_entries.html', entries=entries)    
+
+if __name__ == '__main__':
+    app.run()
--- SkyDrive/NLP/FlaskWorks/Source Code/HelloFlask - Copy.py
+++ SkyDrive/NLP/FlaskWorks/Source Code/HelloFlask - Copy.py
@@ -16,16 +16,27 @@ AO_sCompelationSite  =  AO_ROOT_PATH + AO_PROJECT_NAME + '\\'
 AO_sModulesPath      =  AO_sCompelationSite + 'Source Code'
 
 from werkzeug.wrappers import Request, Response
+from flask import Flask, request, session, g, redirect, url_for, \
+abort, render_template, flash
+# configuration
+DEBUG = True
+SECRET_KEY = ’development key’
+USERNAME = ’admin’
+PASSWORD = ’default’
+# create our little application :)
+app = Flask(__name__)
+app.config.from_object(__name__)
+
 import sys
 sys.path.append(AO_sCommonCode)
 import AO_mOpinionWords
 
 @Request.application
-def application(request):
+def AO_lApplication(request):
     AO_sDocument= 'Not a single very charming fat cat sat on the mat.'
     AO_lOpinion = AO_mOpinionWords.AO_lAssessOpinion(AO_sDocument)
     return Response(AO_lOpinion)
 
 if __name__ == '__main__':
     from werkzeug.serving import run_simple
-    run_simple('localhost', 4000, application)
+    run_simple('localhost', 4000, AO_lApplication)
