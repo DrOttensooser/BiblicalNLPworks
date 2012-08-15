281c8b678c4091f01acd3d78916243d0d365fc7b
DrOttensooser <avner.ottensooser@gmail.com>
2012-08-15 23:52:59 +1000

    Add Horizontal lines arround 10ers

--- SkyDrive/NLP/interfaces/Source Code/AO_XLS.py
+++ SkyDrive/NLP/interfaces/Source Code/AO_XLS.py
@@ -9,7 +9,7 @@ __email__       = 'avner.ottensooser@gmail.com'
 AO_sSource      = "C:\Users\Avner\SkyDrive\NLP\TicketNLPWorks\Data\Excel\Book2.xls"
 AO_sDestinatiom = "C:\Users\Avner\SkyDrive\NLP\TicketNLPWorks\Data\Excel\Book2 - NLP.xls"
 
-
+# the two excle pakages
 from xlrd import open_workbook,cellname
 from xlwt import Workbook
 
@@ -25,25 +25,50 @@ except:
     raise
 
 def main():
-    AO_xlsSourceBook = open_workbook(AO_sSource)
+
+    # open the source spreadsheet
+    try:
+        AO_xlsSourceBook = open_workbook(AO_sSource)
+    except IOError as e:
+        print "I/O error({0}): {1}".format(e.errno, e.strerror)
+        raise
+    
+    # open the destination spreadsheet
     AO_xlsDestinationBook = Workbook()
 
-    for k in range (0,1):
+    # for the two sheets in the source 
+    for k in range (0,2):
+
+        # find a worksheet
         AO_xlsSourceSeet  = AO_xlsSourceBook.sheet_by_index(k)
+
+        # create a new corseponding worksheet with the same name
         AO_xlsDestinationSeet = AO_xlsDestinationBook.add_sheet(AO_xlsSourceSeet.name)
         
-
+        # for all the rows in the source workseet
         for row_index in range(AO_xlsSourceSeet.nrows):
+
+            # for all the colouns in the worksheet
             for col_index in range(AO_xlsSourceSeet.ncols):
+
+                # Add a cell to the destination worksheet
                 AO_xlsDestinationSeet.write(row_index, col_index, AO_xlsSourceSeet.cell(row_index,col_index).value)
+
+                # if the cell has oinion, analyse it
                 if col_index == 1:
                     AO_lOpinion=  AO_client.service.opinionAssesmentRequest(AO_xlsSourceSeet.cell(row_index,col_index).value)[0]
+
+            # place the analisys as the last columns in the row        
             for j in range(0,len(AO_lOpinion)-1):
                 AO_xlsDestinationSeet.write(row_index, col_index +1+ j,AO_lOpinion[j])
     
-
-
-
-    AO_xlsDestinationBook.save(AO_sDestinatiom)
+    # Finaly, try to save the destination WorkBook
+    try:
+        AO_xlsDestinationBook.save(AO_sDestinatiom)
+    except IOError as e:
+        print "I/O error({0}): {1}".format(e.errno, e.strerror)
+        raise
+    
+    
 if __name__ == '__main__':
     main()
